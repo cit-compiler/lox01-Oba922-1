5e388650476e5792bb06db58fb5c6f163199cafd
Oba922 <ciyu23074@g.nihon-u.ac.jp>
2025-12-02 10:29:01 +0900

    add string method

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -97,7 +97,8 @@ class Scanner {
       case '\n':
         line++;
         break;
-          
+      case '"': string();
+        break;
       default:
         Lox.error(line,"Unexpected character.");
         break;
@@ -129,4 +130,20 @@ class Scanner {
     if (isAtEnd()) return '\0';
     return source.charAt(current);
   }
+   private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    advance();
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+
 }
