90311a4adfd58d7dc78cf90f9fc4bdae85aa2480
Oba922 <ciyu23074@g.nihon-u.ac.jp>
2025-12-16 11:10:56 +0900

    Parser.java and Lox.java

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -42,6 +42,13 @@ public class Lox {
     List<Token> tokens = scanner.scanTokens();
 
     // For now, just print the tokens.
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
+
+    // Stop if there was a syntax error.
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expression));
     for (Token token : tokens) {
       System.out.println(token);
     }
@@ -55,5 +62,12 @@ public class Lox {
     System.err.println(
         "[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 }
