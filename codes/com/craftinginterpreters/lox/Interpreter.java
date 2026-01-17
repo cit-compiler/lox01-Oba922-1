015d9359d9ae037de5d3f4b8c59b0a69a3e6f05b
Oba922 <ciyu23074@g.nihon-u.ac.jp>
2026-01-17 18:39:09 +0900

    update Interpreter.java and Lox.java

--- codes/com/craftinginterpreters/lox/Interpreter.java
+++ codes/com/craftinginterpreters/lox/Interpreter.java
@@ -1,7 +1,16 @@
 package com.craftinginterpreters.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
-    @Override
+  void interpret(Expr expression) { 
+    try {
+      Object value = evaluate(expression);
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
+
+  @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
   }
@@ -50,6 +59,20 @@ class Interpreter implements Expr.Visitor<Object> {
     return a.equals(b);
   }
 
+   private String stringify(Object object) {
+    if (object == null) return "nil";
+
+    if (object instanceof Double) {
+      String text = object.toString();
+      if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+
+    return object.toString();
+  }
+
   private Object evaluate(Expr expr) {
     return expr.accept(this);
   }
@@ -83,8 +106,8 @@ class Interpreter implements Expr.Visitor<Object> {
         if (left instanceof String && right instanceof String) {
           return (String)left + (String)right;
         }
-
-        break;
+        throw new RuntimeError(expr.operator,
+            "Operands must be two numbers or two strings.");
       case SLASH:
         checkNumberOperands(expr.operator, left, right);
         return (double)left / (double)right;
